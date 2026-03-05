9579a2c9c8bd65bebcca9d3099fe459a783c33db
Mario Gemoll <mariogemoll@mailbox.org>
2026-03-05 14:54:45 +0100

    Add notebooks for CartPole REINFORCE

---
+++ py/cartpole_reinforce.py
@@ -0,0 +1,13 @@
+# SPDX-FileCopyrightText: 2026 Mario Gemoll
+# SPDX-License-Identifier: 0BSD
+
+from pathlib import Path
+
+import anywidget
+import traitlets
+
+
+class CartPoleReinforceVisualization(anywidget.AnyWidget):
+    _esm = Path(__file__).parent / "dist" / "cartpole-reinforce-visualization.js"
+    _css = (Path(__file__).parent.parent / "ts" / "reinforcement-learning.css").read_text()
+    weights_base64 = traitlets.Unicode("").tag(sync=True)
